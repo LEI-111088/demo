a1106dba1e1595b673345021dcc75f62fff4a631
cmsca3 <cmsca3@iscte-iul.pt>
2025-12-05 15:36:01 +0000

    Corrigi os 3 referentes à Parte 1A - 3 tests passed!

--- src/test/java/iscteiul/ista/demo/MainPageTest.java
+++ src/test/java/iscteiul/ista/demo/MainPageTest.java
@@ -5,6 +5,13 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static com.codeborne.selenide.Condition.attribute;
@@ -14,6 +21,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainPageTest {
 
     MainPage mainPage = new MainPage();
+    //WebDriver driver = new ChromeDriver();
 
     @BeforeAll
     public static void setUpAll() {
@@ -28,23 +36,39 @@ public class MainPageTest {
 
     @Test
     public void search() {
+        wait(5000);
         mainPage.searchButton.click();
-        $("[data-test='search-input']").sendKeys("Selenium");
+        wait(3000);
+        $("[data-test-id='search-input']").sendKeys("Selenium");
+        wait(3000);
         $("button[data-test='full-search-button']").click();
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+        wait(3000);
+        $("input[data-test-id='search-input']").shouldHave(attribute("value", "Selenium"));
     }
 
     @Test
     public void toolsMenu() {
         mainPage.toolsMenu.click();
-        $("div[data-test='main-submenu']").shouldBe(visible);
+        wait(3000);
+        $("div[data-test='main-submenu-suggestions']").shouldBe(visible);
     }
 
     @Test
     public void navigationToAllTools() {
         mainPage.seeDeveloperToolsButton.click();
+        wait(3000);
         mainPage.findYourToolsButton.click();
+        wait(3000);
         $("#products-page").shouldBe(visible);
         assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+    }
+
+    public void wait(int numMillis) {
+        try{
+            Thread.sleep(numMillis);
+        }
+        catch (Exception e){
+            System.out.println("Ignoring exception during sleep: " + e.getMessage());
+        }
     }
 }
